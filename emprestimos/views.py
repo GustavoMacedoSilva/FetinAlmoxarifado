f3695afe6805771a9b6bdee0b1f88a38f75135a0
Eduardo1865 <eduardo.a@gec.inatel.br>
2024-06-01 14:16:16 -0300

    modified:   emprestimos/templates/componente_emprestimo_recente.html 	modified:   emprestimos/templates/componente_feed.html 	modified:   emprestimos/views.py

--- emprestimos/views.py
+++ emprestimos/views.py
@@ -7,16 +7,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Emprestimo
-from autenticacao.models import User
+from autenticacao.models import User,Aluno
 
 # Create your views here.
 
 def EmprestimoView(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    user = User.objects.filter(
-        Q(username__icontains=q) 
+    aluno= Aluno.objects.filter(
+        Q(user__username__icontains=q) 
     )[:8] #get(esp),filter(multiecificoplos especifico),exclude
 
-    context = {'user': user}
+    context = {'aluno': aluno}
     return render(request, 'emprestimos_almoxarife.html', context)
     
