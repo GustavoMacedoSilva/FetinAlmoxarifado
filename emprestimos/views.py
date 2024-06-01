fe501c22ce4e8899b29cc1d2792b8a31816c3f83
Eduardo1865 <eduardo.a@gec.inatel.br>
2024-06-01 16:40:01 -0300

    modified:   db.sqlite3 	modified:   emprestimos/templates/componente_feed.html 	modified:   emprestimos/views.py

--- emprestimos/views.py
+++ emprestimos/views.py
@@ -7,16 +7,29 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Emprestimo
-from autenticacao.models import User,Aluno
+from autenticacao.models import User, Aluno
+from inventario.models import Componente, Equipamento, Emprestimo_has_components
 
 # Create your views here.
 
 def EmprestimoView(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    aluno= Aluno.objects.filter(
-        Q(user__username__icontains=q) 
-    )[:8] #get(esp),filter(multiecificoplos especifico),exclude
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    alunos = Aluno.objects.filter(
+        Q(user__username__icontains=q)
+    )[:8]
 
-    context = {'aluno': aluno}
+    # Coletar todos os empréstimos relacionados aos alunos filtrados
+    emprestimos = Emprestimo.objects.filter(aluno__in=alunos)
+
+    # Coletar todos os equipamentos relacionados a esses empréstimos
+    equipamentos = Equipamento.objects.filter(emprestimo__in=emprestimos)
+
+    componentes = Emprestimo_has_components.objects.filter(emprestimo__in=emprestimos)
+
+    context = {
+        'alunos': alunos,
+        'equipamentos': equipamentos,
+        'emprestimos': emprestimos,
+        'componentes': componentes,
+    }
     return render(request, 'emprestimos_almoxarife.html', context)
-    
