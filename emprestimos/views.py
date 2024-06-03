fdafa0199efead018c6becedfbe32be4077a3679
Eduardo1865 <eduardo.a@gec.inatel.br>
2024-06-03 19:50:05 -0300

    modified:   emprestimos/templates/componente_emprestimo_recente.html 	modified:   emprestimos/templates/detalhes.html 	modified:   emprestimos/templates/emprestimos_almoxarife.html     modified:   emprestimos/views.py

--- emprestimos/views.py
+++ emprestimos/views.py
@@ -4,34 +4,32 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Emprestimo
 from autenticacao.models import User, Aluno
 from inventario.models import Componente, Equipamento, Emprestimo_has_components
 
-# Create your views here.
-
 def EmprestimoView(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
-    alunos = Aluno.objects.filter(
-        Q(user__username__icontains=q)
-    )[:8]
+    alunos = Aluno.objects.filter(Q(user__username__icontains=q))[:8]
 
-    # Coletar todos os empréstimos relacionados aos alunos filtrados
-    emprestimos = Emprestimo.objects.filter(aluno__in=alunos)
 
-    # Coletar todos os equipamentos relacionados a esses empréstimos
+    emprestimos = Emprestimo.objects.all().order_by('-data_de_retirada')
+
+
     equipamentos = Equipamento.objects.filter(emprestimo__in=emprestimos)
-
     componentes = Emprestimo_has_components.objects.filter(emprestimo__in=emprestimos)
+
+    alunos_com_emprestimos = Aluno.objects.filter(emprestimo__isnull=False).distinct()
+    alunos_com_emprestimos = sorted(alunos_com_emprestimos, key=lambda aluno: aluno.emprestimo_set.order_by('-data_de_retirada').first().data_de_retirada, reverse=True)
 
     context = {
         'alunos': alunos,
         'equipamentos': equipamentos,
         'emprestimos': emprestimos,
         'componentes': componentes,
+        'alunos_com_emprestimos': alunos_com_emprestimos,
     }
     return render(request, 'emprestimos_almoxarife.html', context)
 
@@ -41,7 +39,7 @@ def EmprestimoDetalhes(request, pk):
     except Aluno.DoesNotExist:
         return redirect('home_page')
     
-    emprestimos = Emprestimo.objects.filter(aluno=aluno)
+    emprestimos = Emprestimo.objects.filter(aluno=aluno).order_by('-data_de_retirada')
 
     if not emprestimos.exists():
         return redirect('home_page')
@@ -49,10 +47,15 @@ def EmprestimoDetalhes(request, pk):
     equipamentos = Equipamento.objects.filter(emprestimo__in=emprestimos)
     componentes = Emprestimo_has_components.objects.filter(emprestimo__in=emprestimos)
 
+    # Ordenar alunos com base no empréstimo mais recente
+    alunos_com_emprestimos = Aluno.objects.filter(emprestimo__isnull=False).distinct()
+    alunos_com_emprestimos = sorted(alunos_com_emprestimos, key=lambda aluno: aluno.emprestimo_set.order_by('-data_de_retirada').first().data_de_retirada, reverse=True)
+
     context = {
         'aluno': aluno,
         'equipamentos': equipamentos,
         'emprestimos': emprestimos,
         'componentes': componentes,
+        'alunos_com_emprestimos': alunos_com_emprestimos,
     }
     return render(request, 'detalhes.html', context)
