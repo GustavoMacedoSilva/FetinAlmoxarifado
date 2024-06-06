3c265d350592805d372c4e7d78e33d5444e28d2f
Eduardo1865 <eduardo.a@gec.inatel.br>
2024-06-06 16:16:24 -0300

    modified:   db.sqlite3 	new file:   emprestimos/migrations/0002_alter_emprestimo_data_de_retirada.py 	modified:   emprestimos/models.py

--- emprestimos/models.py
+++ emprestimos/models.py
@@ -6,7 +6,7 @@ from autenticacao.models import Aluno, Funcionario
 class Emprestimo(models.Model):
     id = models.BigAutoField(primary_key=True, unique=True)
     estado = models.CharField(max_length=50)
-    data_de_retirada = models.DateField(verbose_name='Data de Retirada')
+    data_de_retirada = models.DateField(verbose_name='Data de Retirada', auto_now_add=True)
     data_de_devolucao = models.DateField(verbose_name='Data de Devolução')
     aluno = models.ForeignKey(Aluno, on_delete=models.PROTECT)
     funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
