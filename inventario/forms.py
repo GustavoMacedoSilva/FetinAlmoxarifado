f5b9073ce9488a6b286a134f6743988f2b43dbb7
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2024-06-11 16:56:28 -0300

    Inicio da implementação dos novos forms

---
+++ inventario/forms.py
@@ -0,0 +1,12 @@
+from .models import Equipamento
+from django.forms import ModelForm
+from django import forms
+
+class createEquipamentoForm(ModelForm):
+    class Meta:
+        model = Equipamento
+        fields = []
+    id = forms.IntegerField()
+    nome = forms.CharField(max_length=50)
+    descricao = forms.CharField(max_length=500)
+    localizacao = forms.CharField(max_length=50)
