612aa6ec401806d94fc9df6ae8cd818134cdb794
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2024-08-05 19:27:37 -0300

    Update no test de cards

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True, unique=True)
     email= models.EmailField(unique=True, null=True)
     is_aluno = models.BooleanField(default=False)
-    is_funcionario =models.BooleanField(default=False)
+    is_funcionario = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
