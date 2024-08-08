7dd5c6e1e0d9436589f48df5ac18974b9c05a35e
Eduardo1865 <eduardo.a@gec.inatel.br>
2024-08-08 11:01:32 -0300

    modified:   emprestimos/urls.py

--- emprestimos/urls.py
+++ emprestimos/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import EmprestimoView, EmprestimoDetalhes, createEmprestimo, testView, deleteEmprestimo
+from .views import EmprestimoView, EmprestimoDetalhes, createEmprestimo, testView, deleteEmprestimo, editEmprestimo
 
 
 urlpatterns = [
     path('',EmprestimoView, name='emprestimo'),
     path('aluno/<int:pk>/',EmprestimoDetalhes, name='detalhes' ),
+
     path('cadastrar/', createEmprestimo, name='cadastrar-emprestimo'),
-    path('test/', testView, name='test'),
     path('delete-emprestimo/<str:pk>', deleteEmprestimo, name="delete-emprestimo"),
+    path('edit-emprestimo/<str:pk>', editEmprestimo, name="edit-emprestimo"),
+    
+    path('test/', testView, name='test'),
 ]
