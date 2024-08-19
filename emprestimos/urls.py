61517ec64f235656943ca89fc69e362a33d7251b
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2024-08-19 15:02:14 -0300

    Instalação de package para tasks periodicas

--- emprestimos/urls.py
+++ emprestimos/urls.py
@@ -5,10 +5,7 @@ from .views import EmprestimoView, EmprestimoDetalhes, createEmprestimo, testVie
 urlpatterns = [
     path('',testView, name='emprestimo'),
     path('aluno/<int:pk>/',EmprestimoDetalhes, name='detalhes' ),
-
     path('cadastrar/', createEmprestimo, name='cadastrar-emprestimo'),
     path('delete-emprestimo/<str:pk>', deleteEmprestimo, name="delete-emprestimo"),
     path('edit-emprestimo/<str:pk>', editEmprestimo, name="edit-emprestimo"),
-    
-    
 ]
