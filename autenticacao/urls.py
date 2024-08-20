9f62c8f9b18f6e0bb762d87a4194d1bdd904de32
Gustavo M Silva <silvamacedo.gustavo2004@gmail.com>
2024-08-20 15:11:12 -0300

    Botao unico para login

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import loginAlmoxarife, loginAluno,logoutUser, createUserAluno, createUserAlmoxarife
+from .views import loginUser, logoutUser, createUserAluno, createUserAlmoxarife
 
 urlpatterns = [
     ##path('endereço/', MinhaView.as_view(), name = 'nome-da-url'), colinha
-    path('almoxarife/', loginAlmoxarife, name='loginAlmoxarife'),
-    path('aluno/', loginAluno, name='loginAluno'),
+    path('login/', loginUser, name='loginUser'),
     path('logout/', logoutUser, name='logout'),
     path('registrarUserAluno/', createUserAluno, name='userCreation'),
     path('registrarUserFuncionario/', createUserAlmoxarife, name='userCreationFuncionario'),
